4875590994f1080a7351c30688f89cca4d33be27
rainli <2977513360@qq.com>
2024-06-08 15:08:56 +0800

    暂存一下

---
+++ src/views/Content.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="content">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="250px">
+          <el-menu :default-openeds="['1', '3']">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>组件</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">
+                  <router-link to="/content/componenta">组件A</router-link>
+                </el-menu-item>
+                <el-menu-item index="1-2">
+                  <router-link to="/content/componentb">组件B</router-link>
+                </el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="2-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="3-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="3-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Content',
+  }
+</script>
+<style>
+  a,
+  a.router-link-active,
+  a:hover,
+  a:focus {
+    text-decoration: none;
+    color: inherit;
+    /* 保持文字颜色不变 */
+  }
+
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 550px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 550px;
+  }
+</style>
